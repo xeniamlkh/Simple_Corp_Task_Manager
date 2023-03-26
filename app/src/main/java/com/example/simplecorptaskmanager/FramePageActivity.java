1286e22ee51f3707f941956ccc97300c403dbea9
xeniamlkh <xenia.melikhova@gmail.com>
2023-03-26 20:07:53 +0300

    Add: options menu, team list fragment

--- app/src/main/java/com/example/simplecorptaskmanager/FramePageActivity.java
+++ app/src/main/java/com/example/simplecorptaskmanager/FramePageActivity.java
@@ -2,6 +2,7 @@ package com.example.simplecorptaskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class FramePageActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class FramePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_page);
+
+        Intent intent = getIntent();
+        String flag = intent.getStringExtra("FLAG");
+
+        if (flag.equals("RunTeamListFragment")){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame_layout_for_fragments, new TeamListFragment())
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/example/simplecorptaskmanager/TodayPageActivity.java
+++ app/src/main/java/com/example/simplecorptaskmanager/TodayPageActivity.java
@@ -1,8 +1,12 @@
 package com.example.simplecorptaskmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class TodayPageActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class TodayPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.our_team_btn:
+                Intent intent = new Intent(TodayPageActivity.this,
+                        FramePageActivity.class);
+                intent.putExtra("FLAG", "RunTeamListFragment");
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
