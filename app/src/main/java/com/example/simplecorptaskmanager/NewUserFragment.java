0f888184e114dafd7713d076f34997c3aa482b3e
xeniamlkh <xenia.melikhova@gmail.com>
2023-03-26 20:55:46 +0300

    Add: new user fragment

--- app/src/main/java/com/example/simplecorptaskmanager/NewUserFragment.java
+++ app/src/main/java/com/example/simplecorptaskmanager/NewUserFragment.java
@@ -1,6 +1,5 @@
 package com.example.simplecorptaskmanager;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,16 +10,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TeamListFragment#newInstance} factory method to
+ * Use the {@link NewUserFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TeamListFragment extends Fragment {
+public class NewUserFragment extends Fragment {
 
-    private Button btnAddNewUser;
-    private Button btnReturnToTodayPage;
+    private EditText userName;
+    private EditText jobTitle;
+    private Button btnSaveUser;
+    private Button btnCancel;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -31,7 +33,7 @@ public class TeamListFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public TeamListFragment() {
+    public NewUserFragment() {
         // Required empty public constructor
     }
 
@@ -41,11 +43,11 @@ public class TeamListFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TeamListFragment.
+     * @return A new instance of fragment NewUserFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TeamListFragment newInstance(String param1, String param2) {
-        TeamListFragment fragment = new TeamListFragment();
+    public static NewUserFragment newInstance(String param1, String param2) {
+        NewUserFragment fragment = new NewUserFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -66,32 +68,33 @@ public class TeamListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team_list, container, false);
+        return inflater.inflate(R.layout.fragment_new_user, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        btnAddNewUser = view.findViewById(R.id.btn_add_new_user);
-        btnReturnToTodayPage = view.findViewById(R.id.btn_return);
+        userName = view.findViewById(R.id.user_name_edit_text);
+        jobTitle = view.findViewById(R.id.job_title_edit_text);
+        btnSaveUser = view.findViewById(R.id.btn_save_user);
+        btnCancel = view.findViewById(R.id.btn_cancel_user);
 
-        btnAddNewUser.setOnClickListener(new View.OnClickListener() {
+        btnSaveUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO create and save a user
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 requireActivity()
                         .getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.frame_layout_for_fragments, new NewUserFragment())
+                        .replace(R.id.frame_layout_for_fragments, new TeamListFragment())
                         .commit();
-            }
-        });
-
-        btnReturnToTodayPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), TodayPageActivity.class);
-                startActivity(intent);
             }
         });
     }
