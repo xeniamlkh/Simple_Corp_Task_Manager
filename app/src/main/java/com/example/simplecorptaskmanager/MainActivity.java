6d8df212878bb70f3f09e4527b7b9a90221b7678
xeniamlkh <xenia.melikhova@gmail.com>
2023-03-26 19:30:38 +0300

    Added authentication activity, frame activity for fragments and main screen - TodayPageActivity

--- app/src/main/java/com/example/simplecorptaskmanager/MainActivity.java
+++ app/src/main/java/com/example/simplecorptaskmanager/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.simplecorptaskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAuth = findViewById(R.id.btn_authentication_main_activity);
+        buttonAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TodayPageActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/simplecorptaskmanager/TodayPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.simplecorptaskmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodayPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today_page);
+    }
+}
